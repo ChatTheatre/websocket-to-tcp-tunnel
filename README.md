ed8faf4d52e6f21586ef05bc30591188bcb7c848
Marshall Davis <toothlessrebel@gmail.com>
2017-10-05 22:54:01 -0400

    Generalizes the Package
     Removes references to Skotos and The Eternal City. Adds further and
    more in depth documentation with code and README.md.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# TEC Tunnel
-WebSocket tunnel proof of concept for TEC.
+# WebSocket to TCP Tunnel
+A basic relay between a listening WebSockets server and TCP server.  
+Data between both sockets is simply echoed to the other.
+
+## Requirements
+* Node.js - The WebSocket server runs via Node.js
+* npm - npm is used to install package dependencies.
+
+## Installation
+Clone the repository with `git clone https://github.com/ToothlessRebel/websocket-to-tcp-tunnel.git`.  
+From the new repository's root directory run `npm install`.
+
+## Starting Relay
+`main.js` has three configuration variables in the first few lines. These include the 
+port numbers both servers, and the URL for the TCP server to be communicated with.  
+To start the relay use `node main.js &` from the root directory of the cloned repository.
--- main.js
+++ main.js
@@ -2,15 +2,13 @@
  * Configuration variables.
  */
 
+// The port numbers to be used for the relay.
 var ports = {
-    listen: 6001,
-    send: 6730
+    listen: 6001, // The port the WebSocket is listening on.
+    send: 6730    // The port on which to connect to the game.
 };
+// The URL on which the game is located.
 var forward_url = 'tec.skotos.net';
-
-/**
- * End Configuration
- */
 
 /**
  * Begin tunnel code.
@@ -22,23 +20,27 @@ var webServer = new WebSocketServer({
 });
 // Listen to WebSocket
 webServer.on('connection', function (server) {
-    console.log('New server spawned.');
+    console.log('Starting relay for new client.');
 
     // Create connection to game socket.
     var tunnel = require('net').Socket();
     tunnel.connect(ports.send, forward_url, function () {
         console.log('Opened tunnel to TEC.');
     });
+    // When the game socket receives new data relay it to the WebSocket.
     tunnel.on('data', function (data) {
         server.send(data);
     });
+    // Should the TCP tunnel become inaccessible, free it on this end.
     tunnel.on('close', function () {
         console.log('Tunnel collapsed.');
         tunnel.destroy();
     });
 
+    // When the WebSocket receives data relay it to the TCP tunnel.
     server.on('message', function (message) {
         tunnel.write(message);
     });
 });
+// Start-up message.
 console.log('WebSockets listening on port ' + ports.listen);
