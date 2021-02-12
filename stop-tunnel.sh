449ee4013bc4fdf00cbb11d4af08167bd7c60f79
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-02-12 10:21:35 +0000

    Try for Mac-and-Ubuntu-compatible stop script using pkill

--- stop-tunnel.sh
+++ stop-tunnel.sh
@@ -4,14 +4,17 @@ set -e
 set -x
 
 HOME=/usr/local/websocket-to-tcp-tunnel
-PID=`cat $HOME/tunnel.pid`
 
-if [ -f "$HOME/tunnel.pid" ] && [ -e /proc/$PID ]; then
-    kill -9 `cat $HOME/tunnel.pid`
+if [ -f "$HOME/tunnel.pid" ]; then
+    PID=`cat $HOME/tunnel.pid`
+    if [ -e /proc/$PID ]; then
+        kill -9 $PID
+    fi
 fi
 
-ps aux | grep "Relay.js" | grep -v grep | cut -c 9-14 | xargs kill -9
+pkill -f "Relay.js" -9 || echo Ok...
+pkill -f "main.js" -9 || echo Ok...
 
 cat <<EndOfMessage
-Please note that cron will automatically restart the tunnels constantly, so just killing them will NOT permanently stop them.
+Please note that cron may automatically restart the tunnels constantly, so just killing them may NOT permanently stop them.
 EndOfMessage
